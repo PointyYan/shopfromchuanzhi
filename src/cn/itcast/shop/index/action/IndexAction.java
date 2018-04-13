fa842bac0aacf21bb770ed11d6588faf9606729e
Vapor <yjq97@outlook.com>
2018-04-13 22:41:31 +0800

    init and some xml

---
+++ src/cn/itcast/shop/index/action/IndexAction.java
@@ -0,0 +1,21 @@
+package cn.itcast.shop.index.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+/**
+ * 首页访问Action
+ * @author yan
+ *
+ */
+
+public class IndexAction extends ActionSupport{
+
+	/**
+	 * 执行的访问首页的方法
+	 */
+	public String execute() {
+		
+		return NONE;
+		
+	}
+}
